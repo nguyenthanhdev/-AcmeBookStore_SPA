179c8bc4ebe79bb6afb3257633c7ef0d76c465b9
nguyenthanhltv <thanh.tn@eiu.edu.vn>
2023-04-03 01:45:09 +0700

    #init project

---
+++ src/app/book/book.component.ts
@@ -0,0 +1,59 @@
+import { ListService, PagedResultDto } from '@abp/ng.core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { BookDto, BookService, bookTypeOptions } from '@proxy/books';
+
+@Component({
+  selector: 'app-book',
+  templateUrl: './book.component.html',
+  styleUrls: ['./book.component.scss'],
+  providers: [ListService],
+})
+export class BookComponent implements OnInit {
+  book = { items: [], totalCount: 0 } as PagedResultDto<BookDto>;
+  isModalOpen = false; // add this line
+  form: FormGroup; // add this line
+  // add bookTypes as a list of BookType enum members
+  bookTypes = bookTypeOptions;
+
+  constructor(public readonly list: ListService,
+    private bookService: BookService,
+    private fb: FormBuilder) { }
+
+  ngOnInit() {
+    const bookStreamCreator = (query) => this.bookService.getList(query);
+
+    this.list.hookToQuery(bookStreamCreator).subscribe((response) => {
+      this.book = response;
+    });
+  }
+
+  // add new method
+  createBook() {
+    this.buildForm(); // add this line
+    this.isModalOpen = true;
+  }
+
+  // add buildForm method
+  buildForm() {
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      type: [null, Validators.required],
+      publishDate: [null, Validators.required],
+      price: [null, Validators.required],
+    });
+  }
+
+   // add save method
+   save() {
+    if (this.form.invalid) {
+      return;
+    }
+
+    this.bookService.create(this.form.value).subscribe(() => {
+      this.isModalOpen = false;
+      this.form.reset();
+      this.list.get();
+    });
+  }
+}
